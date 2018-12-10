f43b394e1811e3a8bb108377b1085a8caedff7c3
Antoine Grondin <antoinegrondin@gmail.com>
2018-12-09 21:40:15 -0800

    remove commented out grpc compiler

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -8,6 +8,3 @@ for d in $(find ${ROOT_IDL_DIR} -name "*.proto" | xargs -n1 dirname | sort -u); 
     compile_cmd="protoc --go_out=plugins=grpc:go/src -I $ROOT_IDL_DIR $d/*.proto"
     docker run --rm -v $ROOT_DIR:/build/src/ -w=/build/src/ $GRPCGO_COMPILER $compile_cmd
 done
-
-# GRPCCSHARP_COMPILER="grpc/csharp:1.0"
-# docker run --rm -v $ROOT_DIR:/build/src/ -w=/build/src/$ROOT_IDL_DIR $GRPCCSHARP_COMPILER $IDL_COMPILE_CMD
